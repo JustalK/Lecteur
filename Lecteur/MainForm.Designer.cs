ce02c690dcc3963ed612884349fc668228c73e78
Latsuj <justal.kevin@gmail.com>
2015-10-27 00:34:01 +0100

    [0] Starting the mp3 project

---
+++ Lecteur/MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Latsuj
+ * Date: 26/10/2015
+ * Time: 20:25
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+ 
+using System.IO;
+using System.Diagnostics;
+namespace Lecteur
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+			this.label1.Location = new System.Drawing.Point(2, 445);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(580, 2);
+			this.label1.TabIndex = 0;
+			// 
+			// label2
+			// 
+			this.label2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.Location = new System.Drawing.Point(482, 449);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "Justal \"Latsuj\" K.";
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(584, 466);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "MainForm";
+			this.Text = "Lecteur";
+			this.ResumeLayout(false);
+			
+			string[] array1 = Directory.GetFiles(@"C:\");
+		
+			var position = 0;
+			foreach (string name in array1)
+			{
+				var labeltmp = new System.Windows.Forms.Label();
+				labeltmp.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+				labeltmp.Location = new System.Drawing.Point(10, 12+position);
+				labeltmp.AutoSize = true;
+				labeltmp.Text = name;
+				Debug.WriteLine(name+" "+position);
+				position+=40;
+				var linetmp = new System.Windows.Forms.Label();
+				linetmp.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+				linetmp.Location = new System.Drawing.Point(0, position);
+				linetmp.Name = "label1";
+				linetmp.Size = new System.Drawing.Size(600, 2);
+				linetmp.TabIndex = 0;			
+				this.Controls.Add(labeltmp); 	
+				this.Controls.Add(linetmp); 
+			}
+		}
+	}
+}
---
+++ Lecteur/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Latsuj
+ * Date: 26/10/2015
+ * Time: 20:25
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+namespace Lecteur
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{				
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
