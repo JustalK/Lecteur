37948af15c8d1badfd482aa30c7c8fd9addc291a
Latsuj <justal.kevin@gmail.com>
2015-10-28 01:09:23 +0100

    [2] Can now trigger the event

--- Lecteur/MainForm.Designer.cs
+++ Lecteur/MainForm.Designer.cs
@@ -16,6 +16,7 @@ namespace Lecteur
 {
 	partial class MainForm
 	{
+		
 		/// <summary>
 		/// Designer variable used to keep track of non-visual components.
 		/// </summary>
@@ -23,7 +24,10 @@ namespace Lecteur
 		private System.Windows.Forms.Label label1;
 		private System.Windows.Forms.Label label2;
 		private System.Windows.Forms.Label label3;
-
+		private WMPLib.WindowsMediaPlayer wplayer;	
+		private int statePlayer;
+		private System.Windows.Forms.Label label4;
+		
 		/// <summary>
 		/// Disposes resources used by the form.
 		/// </summary>
@@ -40,16 +44,41 @@ namespace Lecteur
 
 		void Label3MouseHover(object sender, System.EventArgs e)
 		{
-			this.label3.BackColor = System.Drawing.Color.Blue;
+		}
+	
+		void Label3MouseUp(object sender, System.EventArgs e)
+		{
+			if(statePlayer!=(int)WMPLib.WMPPlayState.wmppsPlaying) {
+				this.label3.BackColor = System.Drawing.Color.Blue;
+				this.wplayer = new WMPLib.WindowsMediaPlayer();
+				
+				this.wplayer.PlayStateChange += new WMPLib._WMPOCXEvents_PlayStateChangeEventHandler(wplayer_PlayStateChange);
+				this.wplayer.URL = "fx2.mp3";
+				this.wplayer.controls.play();
+			}
 		}
 		
 		void Label3MouseLeave(object sender, System.EventArgs e)
 		{
-			this.label3.BackColor = System.Drawing.Color.Red;
-			WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
 
-			wplayer.URL = "fx2.mp3";
-			wplayer.controls.play();
+		}
+		
+		void wplayer_PlayStateChange(int NewState)
+		{
+			statePlayer = NewState;
+			switch (NewState)
+			{
+			    case (int)WMPLib.WMPPlayState.wmppsMediaEnded:
+					this.label3.BackColor = System.Drawing.Color.Red;
+			        Debug.WriteLine("End");
+			        break;
+			    case (int)WMPLib.WMPPlayState.wmppsPlaying:
+			        Debug.WriteLine("Playing");
+			        break;
+			    default:
+			        break;
+			}
+
 		}
 		
 		/// <summary>
@@ -62,6 +91,7 @@ namespace Lecteur
 			this.label1 = new System.Windows.Forms.Label();
 			this.label2 = new System.Windows.Forms.Label();
 			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// label1
@@ -83,15 +113,31 @@ namespace Lecteur
 			// 
 			// label3
 			// 
+			this.label3.BackColor = System.Drawing.Color.Red;
 			this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
 			this.label3.Location = new System.Drawing.Point(2, 2);
 			this.label3.Name = "label3";
 			this.label3.Size = new System.Drawing.Size(580, 51);
 			this.label3.TabIndex = 3;
-			this.label3.Text = "label3";
-			this.label3.BackColor = System.Drawing.Color.Red;
-			this.label3.MouseHover += new System.EventHandler(this.Label3MouseHover);
 			this.label3.MouseLeave += new System.EventHandler(this.Label3MouseLeave);
+			this.label3.MouseHover += new System.EventHandler(this.Label3MouseHover);
+			this.label3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Label3MouseUp);
+			// 
+			// label4
+			// 
+			this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+			| System.Windows.Forms.AnchorStyles.Left) 
+			| System.Windows.Forms.AnchorStyles.Right)));
+			this.label4.BackColor = System.Drawing.Color.Red;
+			this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Pixel, ((byte)(0)));
+			this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+			this.label4.Location = new System.Drawing.Point(12, 2);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(416, 51);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "Title.mp3";
+			this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+			this.label4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Label3MouseUp);
 			// 
 			// MainForm
 			// 
@@ -100,6 +146,7 @@ namespace Lecteur
 			this.BackColor = System.Drawing.SystemColors.Control;
 			this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
 			this.ClientSize = new System.Drawing.Size(584, 466);
+			this.Controls.Add(this.label4);
 			this.Controls.Add(this.label3);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
